8aeca58b3fcb84f291a7bd073ee422adb93e25ee
shilpi <shilpi8624@gmail.com>
2022-02-09 15:17:18 +0530

    start js

---
+++ index.js
@@ -0,0 +1,82 @@
+var a;
+a=20;
+console.log(a);
+console.log(typeof a);
+
+
+var b;
+b = "hello";
+console.log(b)
+console.log(typeof b);
+
+b = true;
+console.log(b);
+console.log(typeof b);
+
+b=null;
+console.log(b);
+console.log(typeof b);
+
+var num =10;
+console.log(num);
+console.log(typeof num);
+
+var flt = 2.5;
+console.log(flt);
+console.log(typeof flt);
+
+var t = true;
+var f = false;
+console.log(t);
+console.log(f);
+console.log(typeof t);
+console.log(typeof f);
+
+var str = 'a';
+console.log(str);
+
+str = "How are you";
+console.log(str);
+
+var text = `hi i hope you guys are able to
+understand the concept`;
+console.log(text);
+
+var num = 100;
+console.log(`half of $ {num} is ${num/2}`);
+
+let l = 100;
+console.log(l);
+
+var a = "bello" //we can do re-declaration and re-initialization both
+console.log(a);
+
+var a = 50;
+console.log(a);
+
+let s = "you are great";
+console.log(s);
+
+//let s = 100; re-declaration not allowed
+s = 100; //but we can do re-initialize 
+console.log(s);
+
+const val = 2;
+console.log(val);
+
+var n = 10;
+for(var i=1; i<=n; i++){
+    console.log(i);
+}
+
+var age = 20;
+if(age<18){
+    console.log("you can not vote")
+}else{
+    console.log("you can vote");
+}
+
+
+
+
+
